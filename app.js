0c2ea988919e77bb4edb7d3935d168a304feccf2
eric.ogieglo <eric.ogieglo@flockit.com.ar>
2024-02-26 20:59:56 -0300

    TOKEN_API_SLACK Bearer

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post('/api_slack', async (req, res) => { // Añade async para permitir el us
             view: modalPayload
         }), {
             headers: {
-                'Authorization': '${SLACK_TOKEN}', // Asegúrate de reemplazar 'xoxb-tu-token-de-bot' con tu token real de Bot
+                'Authorization': `Bearer ${SLACK_TOKEN}`, // Asegúrate de reemplazar 'xoxb-tu-token-de-bot' con tu token real de Bot
                 'Content-Type': 'application/json'
             }
         });
